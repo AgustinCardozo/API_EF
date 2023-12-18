165a520cf4d3b84db46351fe2e4d6c37a59a960b
AgusCardozo <acardozo@andreani.com>
2023-12-18 19:21:47 -0300

    Soporte inicial para pdf

---
+++ src/Models/Factura.cs
@@ -0,0 +1,9 @@
+﻿namespace API_EF.Models
+{
+    public class Factura
+    {
+        public int numeroFactura { get; set; }
+        public DateTime fechaActual { get; set; }
+        public DateTime fechaVencimiento { get; set; }
+    }
+}
